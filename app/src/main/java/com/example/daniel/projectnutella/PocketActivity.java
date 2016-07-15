4951955137010e46c9cf8338ef9d8ad493bf1d02
Santiago Carretero <Santiago Carretero>
2016-07-15 12:03:34 -0300

    Dialog addTransaction (sin seleccion de botones para categorias y botones spend/gain)

--- app/src/main/java/com/example/daniel/projectnutella/PocketActivity.java
+++ app/src/main/java/com/example/daniel/projectnutella/PocketActivity.java
@@ -8,21 +8,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.content.DialogInterface;
+import android.widget.Toast;
 
 import com.example.daniel.projectnutella.adapter.TransactionAdapter;
+import com.example.daniel.projectnutella.data.DbHelper;
 import com.example.daniel.projectnutella.data.Pocket;
 import com.example.daniel.projectnutella.data.Transaction;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class PocketActivity extends AppCompatActivity {
 
     private int pocketId;
+    private AlertDialog dialogAddTrans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +46,7 @@ public class PocketActivity extends AppCompatActivity {
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    addTransaction();
                 }
             });
 
@@ -72,14 +80,53 @@ public class PocketActivity extends AppCompatActivity {
         balanceTV.setText("$"+net);
     }
 
-    /*public void addTransaction(){
+    public void addTransaction(){
         AlertDialog.Builder builder = new AlertDialog.Builder(PocketActivity.this);
         builder.setTitle("Add Transaction");
         builder.setPositiveButton(R.string.add_button, new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface, int id) {
-                if (nameTransaction != null)
+            public void onClick(DialogInterface d, int id) {
+                if (dialogAddTrans != null) {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date date = new Date();
+                    String fecha=dateFormat.format(date);
+                    DbHelper db = new DbHelper(PocketActivity.this);
+                    int nroCat = ((LinearLayout) dialogAddTrans.findViewById(R.id.layoutAmounts)).getChildCount();
+                    int cat = catSelected(nroCat);
+                    if ( cat != -1 && !(((EditText) dialogAddTrans.findViewById(R.id.amountET)).getText().toString().isEmpty())){
+                        Transaction newT = new Transaction(((EditText) dialogAddTrans.findViewById(R.id.amountET)).getText().toString(),fecha,pocketId,cat,isIncome());
+                        db.insertTransaction(newT);
+                    }
+                    else {
+                        Toast toast = Toast.makeText(PocketActivity.this,
+                                getString(R.string.error_add_transaction), Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+
+                }
             }
+        });
+        builder.setView(R.layout.add_transaction);
+        dialogAddTrans = builder.create();
+        dialogAddTrans.show();
+    }
+
+    public int catSelected(int childCount){
+        for (int i = 0; i < childCount - 1; i++){
+            if (dialogAddTrans != null)
+                if (!((LinearLayout) dialogAddTrans.findViewById(R.id.layoutAmounts)).getChildAt(i).isSelected())
+                    return i;
         }
-    }*/
+        return -1;
+    }
+
+    public boolean isIncome() {
+        if (dialogAddTrans != null)
+            if (dialogAddTrans.findViewById(R.id.spendBT).isSelected())
+                return false;
+            else
+                return true;
+        return false; //Retorno falso por defecto
+    }
 
 }
+
